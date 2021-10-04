238d1181d17f1cde2656cdd99583867dc6a8bfe2
Paul Smyth <paul.smyth@griffithuni.edu.au>
2021-10-04 10:34:03 +1000

    Needs work

--- server/routes/groups-read.js
+++ server/routes/groups-read.js
@@ -1,8 +1,20 @@
 module.exports = function(db, app) {
     app.get("/api/groups-read", function(req, res) {
-        const collection = db.collection('groups');
+        
+        //Check user Level
+        const collection = db.collection('current_user');
         collection.find({}).toArray((err, data)=> {
-            res.send(data);
+            
+            if (data[0].role == "Super Administrator" || data[0].role == "Group Administrator") {
+                const collection = db.collection('groups');
+                collection.find({}).toArray((err, datasub)=> {
+                    res.send(datasub);
+                    
+                });
+            } else {
+                res.send({"authenticationStatus":false});
+            }
+
         });
     });
 }
